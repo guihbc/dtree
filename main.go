432c93b08b1063b08f2263f82eeb8dd72d83916b
guihcodes <guihchavesb@gmail.com>
2024-12-01 17:32:02 -0300

    list directory tree

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
 	"strings"
 )
 
@@ -10,9 +14,40 @@ const LAST_ENTRY = "└──"
 const BAR = "│"
 const IDENTATION = 3
 
+var (
+	d = flag.String("d", ".", "directory")
+)
+
+var ignorables = []string{".git"}
+
 func main() {
-	line := build_line(0, "main/", false)
-	fmt.Println(line)
+	flag.Parse()
+	splited_dir := strings.Split(*d, "/")
+	fmt.Println(splited_dir[len(splited_dir)-1])
+	build_directory_tree(0, *d)
+}
+
+func build_directory_tree(depth int, dir string) {
+	entries, _ := os.ReadDir(dir)
+	entries_len := len(entries)
+	last_entry := false
+
+	for i, e := range entries {
+		if slices.Contains(ignorables, e.Name()) {
+			continue
+		}
+
+		if i == entries_len-1 {
+			last_entry = true
+		}
+
+		fmt.Println(build_line(depth, e.Name(), last_entry))
+
+		if e.IsDir() {
+			dir_path := filepath.Join(dir, e.Name())
+			build_directory_tree(depth+1, dir_path)
+		}
+	}
 }
 
 func build_line(depth int, entry_name string, last_entry bool) string {
